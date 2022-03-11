2df91527abb59b755f21eaf08f09d091fa72984c
AnaAlice951 <aasoares55@gmail.com>
2022-03-11 20:31:25 -0300

    first release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# jogo-da-velha
-Api do jogo da velha
+Teste prático - DTI
+
+Descrição do teste:
+Criar uma api para um jogo multiplayer de Jogo da Velha.
+
+🛠️ Abrir e rodar o projeto:
+1. Baixe o código em seu computador.
+2.Abra um terminal no endereço do código baixado
+3. No terminal, digite: npm i express node cors uuid (Esse comando baixará as depências usadas)
+4. Ainda no terminal, digite: npm start
+5. Teste o jogo em aplicativo que permita testar APIs, como Postman e Insomnia
+6. Use o método GET na URL http://localhost:9000/game para criar um novo jogo
+7. Use o método POST na URL http://localhost:9000/game/:id/movement para fazer os movimentos dos player. Onde ":id" é a id retornada em /game
+8. Retorne o movimento do player e o player em formato JSON, como o seguinte exemplo: 
+    {
+        "id" : "fbf7d720-df90-48c4-91f7-9462deafefb8",
+        "player": "X",
+        "position": {
+            "x": 0,
+            "y": 1
+        }
+    }
+6. Divirta-se!
+
+✔️ Técnicas e tecnologias utilizadas
+
+- JavaScript
+- Express
+- Node.js
+- Cors
---
+++ routes.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const routes = express.Router();
+const { v4: uuidv4 } = require("uuid");
+
+//Função para gerar o player inicial
+function getInitialPlayer() {
+  var caracteres = ["X", "O"];
+  return caracteres[Math.floor(Math.random() * caracteres.length)];
+}
+
+//Função para determinar o vencedor
+function winner(position) {
+  if (
+    position[0][2] == position[1][2] &&
+    position[1][2] == position[2][2] &&
+    position[0][2] != ""
+  )
+    return 1;
+  else if (
+    position[0][1] == position[1][1] &&
+    position[1][1] == position[2][1] &&
+    position[0][1] != ""
+  )
+    return 1;
+  else if (
+    position[0][0] == position[1][0] &&
+    position[1][0] == position[2][0] &&
+    position[0][0] != ""
+  )
+    return 1;
+  else if (
+    position[0][2] == position[0][1] &&
+    position[0][1] == position[0][0] &&
+    position[0][2] != ""
+  )
+    return 1;
+  else if (
+    position[1][2] == position[1][1] &&
+    position[1][1] == position[1][0] &&
+    position[1][2] != ""
+  )
+    return 1;
+  else if (
+    position[2][2] == position[2][1] &&
+    position[2][1] == position[2][0] &&
+    position[2][2] != ""
+  )
+    return 1;
+  else if (
+    position[0][2] == position[1][1] &&
+    position[1][1] == position[2][0] &&
+    position[0][2] != ""
+  )
+    return 1;
+  else if (
+    position[2][2] == position[1][1] &&
+    position[1][1] == position[0][0] &&
+    position[2][2] != ""
+  )
+    return 1;
+  else return 0;
+}
+
+//Classe do jogo
+function Game(id, player) {
+  this.id = id;
+  this.player = player;
+  this.position = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  this.remainingMovements = 9;
+}
+
+var games = [];
+
+//Criar uma nova partida e retornar o id da partida criada e o jogador inicial dela.
+routes.post("/game", (req, res) => {
+  newGame = new Game(uuidv4(), getInitialPlayer());
+  games.push(newGame);
+  return res.json({
+    id: newGame.id,
+    firstPlayer: newGame.player,
+  });
+});
+
+//Realiza o movimento de cada jogador.
+routes.post("/game/:id/movement", (req, res) => {
+  for (i = 0; i < games.length; i++) {
+    //Verifica se o ID inserido existe
+    if (games[i].id == req.params.id) {
+      //Verifica se é o turno do player inserido
+      if (games[i].player == req.body.player) {
+        //Verifica se a posição inserida existe
+        if (
+          games[i].position[req.body.position.x][req.body.position.y] ===
+          undefined
+        )
+          return res.json({ msg: "Posição inválida" });
+        //Verifica se a posição inserida está ocupada
+        if (games[i].position[req.body.position.x][req.body.position.y] == "") {
+          //Realiza o movimento do player
+          games[i].position[req.body.position.x][req.body.position.y] =
+            games[i].player;
+          //Verifica se, após a jogada realizada, o jogo possui um vencedor
+          if (winner(games[i].position) == 1) {
+            if (games[i].player == "X") {
+              games.splice(i, 1);
+              return res.json({
+                msg: "Partida finalizada",
+                winner: "X",
+              });
+            } else {
+              games.splice(i, 1);
+              return res.json({
+                msg: "Partida finalizada",
+                winner: "O",
+              });
+            }
+          }
+
+          //Verifica se houve empate
+          if (--games[i].remainingMovements == 0) {
+            games.splice(i, 1);
+            return res.json({
+              status: "Partida finalizada",
+              winner: "Draw",
+            });
+          }
+
+          //Muda o turno do player
+          games[i].player = games[i].player == "X" ? "O" : "X";
+          return res.status(200).send();
+        }
+        return res.json({ msg: "Posição ocupada" });
+      }
+      return res.json({ msg: "Não é turno do jogador" });
+    }
+  }
+  return res.json({ msg: "Partida não encontrada" });
+});
+
+module.exports = routes;
